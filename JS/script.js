048999550c1547e288e0821c64c7b8f86b9003df
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-10 19:19:16 +0530

    reslove undef

--- JS/script.js
+++ JS/script.js
@@ -42,7 +42,7 @@ async function checkIfBgExists(link) {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
             'origin': 'https://krunker.io',
         }, mode: 'no-cors', method: 'GET' }).catch(e => r(undefined));
-        resolve(true);
+        r(true);
     })
 }
 
