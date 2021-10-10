addb23cc2b0e159ce2d4f8e4cb09ab0d2fcc09e9
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-10 19:33:43 +0530

    logging for error check

--- JS/script.js
+++ JS/script.js
@@ -41,7 +41,9 @@ async function checkIfBgExists(link) {
         fetch(link, { headers: {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36',
             'origin': 'https://krunker.io',
-        }, mode: 'no-cors', method: 'GET' }).catch(e => r(undefined));
+        }, mode: 'no-cors', method: 'GET' }).catch(e => r(undefined))
+        .then(x => console.log(x));
+        
         r(true);
     })
 }
